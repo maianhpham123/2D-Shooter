4bb26524aceed05b55a10983a4b377b97d8f730e
maianhpham123 <23021469@vnu.edu.vn>
2024-05-06 14:22:01 +0700

    Initial Commit

---
+++ 2D Shooter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  2D Shooter
+//
+//  Created by Mai Anh Pham on 5/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ 2D Shooter/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  2D Shooter
+//
+//  Created by Mai Anh Pham on 5/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
